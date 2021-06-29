7338dc8a4dd19284458c71aeb453b38688d13139
scothy <sokthy77@hotmail.com>
2021-06-29 16:49:03 +0200

    debut media query

--- src/components/Fiverr_source.jsx
+++ src/components/Fiverr_source.jsx
@@ -15,25 +15,28 @@ function Fiverr () {
             <div className="center_h2">
                 <h2 className='title_freelance'>FREELANCE</h2>
                 <div className="link2">
-                <Link className='linkCreate' to="/Home">
+                    <Link className='linkCreate' to="/Home">
                     HERE CREATE YOUR EVENT
-                </Link>
+                    </Link>
                 </div>
             </div>
-            <div className="allCard">
-            <div className ="column_events"> 
-            <p>All Events By category</p>
-            <p>Build Skills</p>
-            <p>Diversity and inclusion</p>
-            <p>Get Connected</p>
-            <p>Get inspired</p>
-            <p>Freelance</p>
-            </div>
-            <img className="card" src={card1} alt="Logo fiverr" />
-            <img className="card" src={card2} alt="Logo fiverr" />
-            <img className="card" src={card3} alt="Logo fiverr" />
-            </div>
+                <div className="allCard">
+                    <div className ="column_events"> 
+                    <p>All Events By category</p>
+                    <p>Build Skills</p>
+                    <p>Diversity and inclusion</p>
+                    <p>Get Connected</p>
+                    <p>Get inspired</p>
+                    <p>Freelance</p>
+                
+                    <div className="container_card">
+                        <img className="card" src={card1} alt="Logo fiverr" />
+                        <img className="card" src={card2} alt="Logo fiverr" />
+                        <img className="card" src={card3} alt="Logo fiverr" />
+                    </div>
+                </div>
         </div>
+    </div>
 )};
 
 export default Fiverr;

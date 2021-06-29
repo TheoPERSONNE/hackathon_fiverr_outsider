4725a306f65d57473bdd7371b833cd227503d6b7
TheoPERSONNE <theo.personne@live.fr>
2021-06-29 15:05:16 +0200

    amelioration feever

--- src/components/Fiverr_source.jsx
+++ src/components/Fiverr_source.jsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import './Fiverr_source.css';
 // import Cards from './Cards';
 import Logo from './logo_fiverr.png';
+import card1 from './cardun.webp';
+import card2 from './carddeux.webp';
+import card3 from './cardtrois.webp';
 
 function Fiverr () {
     return (
@@ -11,12 +14,13 @@ function Fiverr () {
             <img className="logo" src={Logo} alt="Logo fiverr" />
             <div className="center_h2">
                 <h2 className='title_freelance'>FREELANCE</h2>
-                <div className="link">
-                <Link className='links_create' to="/Home1">
-                    <button className='create_event'>HERE CREATE YOUR EVENT</button>
+                <div className="link2">
+                <Link className='linkCreate' to="/Home">
+                    HERE CREATE YOUR EVENT
                 </Link>
                 </div>
             </div>
+            <div className="allCard">
             <div className ="column_events"> 
             <p>All Events By category</p>
             <p>Build Skills</p>
@@ -25,7 +29,10 @@ function Fiverr () {
             <p>Get inspired</p>
             <p>Freelance</p>
             </div>
-            {/* <Cards /> */}
+            <img className="card" src={card1} alt="Logo fiverr" />
+            <img className="card" src={card2} alt="Logo fiverr" />
+            <img className="card" src={card3} alt="Logo fiverr" />
+            </div>
         </div>
 )};
 

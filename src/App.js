172ad530fc478cca910b9c6c999675d64ba96f01
TheoPERSONNE <theo.personne@live.fr>
2021-06-29 10:50:42 +0200

    debug

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-
-
-import './App.css';
-import Fiverr from './components/Fiverr_source';
-import Home from './components/Home';
-import Finalize from './components/Finalize';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Fiverr from "./components/Fiverr_source";
+import Home from "./components/Home";
+import Finalize from "./components/Finalize";
 
 function App() {
   return (
-    <div className="App">
-    <div>
-
-      <Switch>
-        <Route exact path="/" component={Fiverr} />
-        <Route path="/fiverr" component={Fiverr} />
-        <Route path="/Home" component={Home} />
-        <Route path="/Finalize" component={Finalize} />
-      </Switch>
-    </div>
-    
+    <Switch>
+      <Route exact path="/" component={Fiverr} />
+      <Route path="/fiverr" component={Fiverr} />
+      <Route path="/Home" component={Home} />
+      <Route path="/Finalize" component={Finalize} />
+    </Switch>
   );
 }
 
